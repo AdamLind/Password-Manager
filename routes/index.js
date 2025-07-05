189aff176a2b5165d9c0eb4de816e6bcb9b6eea8
Adam Lind <adamsplind@gmail.com>
2025-07-05 15:23:13 -0600

    change homepage message to give instructions

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! Navigate to /contacts to see the contacts API. You can also use /contacts/:id to get a specific contact by ID.');
 });
 
 router.use('/contacts', require('./contacts'));
